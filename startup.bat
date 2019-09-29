4c4c618d2cfca3c73b12172f6aff0b14593b9e55
U-ultrahorst\elmar <ktye@users.noreply.github.com>
2019-09-29 16:43:55 +0200

    prevent contextmenu on bt3-search, remove font css

--- startup.bat
+++ startup.bat
@@ -5,11 +5,12 @@ REM
 
 REM Set the path to use the msys64 environment for running external programs.
 REM Other environments should work as well.
-set PATH=c:/local/msys64/usr/bin;c:/local/go/bin;c:/local/msys64/home/elmar/bin
+set PATH=c:/local/msys64/usr/bin;c:/go/bin;c:/local/msys64/home/elmar/bin
 
 REM Set the gopath explicitly. This is only needed, to work on go projects.
 REM Environment variables can also be changed interactively. See the env built-in.
 set GOPATH=c:/Users/elmar
+set GO111MODULE=on
 
 REM Start the editor server minimized
 REM This is the binary built from ./editor/editor.
@@ -19,4 +20,3 @@ CALL "cmd /c start /min c:\local\msys64\home\elmar\bin\editor.exe"
 REM Open a new tab in the browser with for this root directory.
 REM start "" "http://127.0.0.1:2017/path/to/root"
 start "" "http://127.0.0.1:2017/c:/local/msys64/home/elmar/src/github.com/ktye"
-
